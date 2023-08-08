dbce5ffa6ae604c8f0f7a56129c5bad2c2c7e35e
MacBook <macbook@MacBooks-MacBook-Pro.local>
2023-08-08 21:14:56 +0100

    please be last update

--- src/apis/api.js
+++ src/apis/api.js
@@ -1,14 +1,12 @@
 import axios from "axios";
 
-const apiKey2 = import.meta.env.VITE_API_KEY;
-
 const weatherApi = axios.create({
   baseURL: "http://api.weatherapi.com/v1",
 });
 
 export const fetchWeather = (area) => {
   return weatherApi
-    .get(`/forecast.json?key=${apiKey2}&q=${area}&days=3`)
+    .get(`/forecast.json?key=${import.meta.env.VITE_API_KEY}&q=${area}&days=3`)
     .then((res) => {
       return res.data;
     })
